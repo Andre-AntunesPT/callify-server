9c611d1e6ad899a41b80f315f741f5255800ce4a
André Antunes <andre.n.antunes@gmail.com>
2022-11-24 15:21:49 +0000

    events model and routes changes

--- models/Event.model.js
+++ models/Event.model.js
@@ -3,8 +3,8 @@ const { Schema, model } = require("mongoose");
 const eventSchema = new Schema({
   title: String,
   description: String,
-  type: { type: String, enum: [elearning, webinar, eventThree] },
-  room: { type: Schema.Types.ObjectId, ref: "Room" },
+  /* type: { type: String, enum: [elearning, webinar] }, */
+  /* room: { type: Schema.Types.ObjectId, ref: "Room" }, */
 });
 
 const Event = model("Event", eventSchema);
