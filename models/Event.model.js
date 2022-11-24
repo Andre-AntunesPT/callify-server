8633df13fd0fc8627b7240c03697dfdb58cbc0f6
Raiza Garcia <raizagarcias@gmail.com>
2022-11-24 14:41:45 +0000

    changes on the event model

--- models/Event.model.js
+++ models/Event.model.js
@@ -1,7 +1,8 @@
 const {Schema, model} = require('mongoose');
 
 const eventSchema = new Schema({
-
+    type: {type: String, enum: [elearning, webinar, eventThree]},
+    room: {type: Schema.Types.ObjectId, ref:'Room'},
 });
 
 const Event = model('Event', eventSchema);
